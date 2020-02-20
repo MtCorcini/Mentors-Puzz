92f0ee3aca458694efcebda2001ea33c3164b928
Mateus Corcini <49539006+MtCorcini@users.noreply.github.com>
2020-02-19 22:38:08 -0300

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public  Transform player;  // reference to the player´s transform. 
+    public float cameraDistance;
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.width / 5) / cameraDistance);
+    }
+
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y,transform.position.z);
+    }
+}
+ 
