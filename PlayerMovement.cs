e66615d40f56756a81d142c422c3b08f5a85ed46
Mateus Corcini <49539006+MtCorcini@users.noreply.github.com>
2020-02-19 22:38:47 -0300

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   
+    public Animator animator;
+    public float runSpeed = 2.7f;
+    public float jumpForce = 200f;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    //public float radius = 0.2f;
+    SpriteRenderer sprite;
+    Rigidbody2D body;
+    float horizontalMove = 0f;
+    bool Jump = false;
+    bool isOnFloor = false;
+
+
+        void Start()
+    {
+            body = GetComponent<Rigidbody2D>();
+            sprite = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+            //animator.SetFloat("Speed", Mathf.Abs(horizontalMove))
+            //isOnFloor = Physics2D.OverlapCircle(groundCheck.position, radius, whatIsGround);
+
+            isOnFloor = Physics2D.Linecast(transform.position, groundCheck.position, whatIsGround);
+
+            if (Input.GetButtonDown("Jump") && isOnFloor == false)
+            {
+                Jump = true;
+            }
+        
+            PlayerAnimation();
+    }
+        void FixedUpdate ()
+        {
+            float move = Input.GetAxis("Horizontal");
+
+            body.velocity = new Vector2(horizontalMove * runSpeed, body.velocity.y);
+
+            if ((move > 0 && sprite.flipX == true) || (move < 0 && sprite.flipX == false))
+            {
+                Flip();
+            }
+
+            if (Jump)
+            {
+                body.AddForce(new Vector2(0f, jumpForce));
+                Jump = false;
+
+            }
+
+
+    }
+        void Flip()
+        {
+            sprite.flipX = !sprite.flipX;
+
+        }
+        void PlayerAnimation ()
+        {
+        animator.SetFloat("velX", Mathf.Abs(body.velocity.x));
+        animator.SetFloat("velY", Mathf.Abs(body.velocity.y));
+
+        }
+
+       
+
+       
+    
+         /*void OnDrawGizmosSelected ()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, radius);
+
+        }*/
+        
+ 
+    }
+   
+
---
+++ Teleportation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject Portal, Player;
+    
+    public static int StartTeleport = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Teleportation.StartTeleport == 1)
+        {
+            Teleportation.StartTeleport = 0;
+            StartCoroutine(Teleport());
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+
+    }
+}
